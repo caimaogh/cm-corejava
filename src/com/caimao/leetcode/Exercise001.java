856c78abd34eb109aaa0537358593a493c115935
caimao <uestc_cm@163.com>
2018-09-05 14:26:18 +0800

    exer001 update

--- src/com/caimao/leetcode/Exercise001.java
+++ src/com/caimao/leetcode/Exercise001.java
@@ -17,17 +17,20 @@ public class Exercise001 {
      * 因为 nums[0] + nums[1] = 2 + 7 = 9 所以返回 [0, 1]
      */
     public static void main(String[] args){
+        Exercise001 ex001 = new Exercise001();
         int[] nums = {8, 3, 8, 9, 7, 9};
         int target = 18;
         try{
-            int[] result = twoSum(nums, target);
+            int[] result = ex001.twoSum(nums, target);
             System.out.println(Arrays.toString(result));
         }catch(IllegalArgumentException e){
             System.out.println(e.getMessage());
         }
+
+        System.out.println(ex001.maxLengthSubString("abcbaefaecdfd"));
     }
 
-    public static int[] twoSum(int[] nums, int target){
+    public int[] twoSum(int[] nums, int target){
         Map<Integer, Integer> map = new HashMap<>();
         int[] result= new int[2];
 
@@ -39,5 +42,23 @@ public class Exercise001 {
             map.put(nums[i], i);
         }
         throw new IllegalArgumentException("没有两个相加等于目标数的");
+    }
+
+    public int maxLengthSubString(String s){
+        int maxLengtn = 0;
+        String subString = "";
+        Map<Character, Integer> indexMap = new HashMap<>();
+        for(int i=0, j=0; j<s.length(); j++){
+            if(indexMap.containsKey(s.charAt(j))){
+                i = Math.max(indexMap.get(s.charAt(j)), i);
+            }
+            maxLengtn = Math.max(maxLengtn, j-i+1);
+            indexMap.put(s.charAt(j), j+1);
+            if(subString.length()<(j-i+1)){
+                subString = s.substring(i, j+1);
+            }
+        }
+        System.out.println("最长子串长度是:"+maxLengtn+",子串:"+subString);
+        return maxLengtn;
     }
 }
