86e74bd29ad233781ac7ea8b498b28ddaf60b0a3
caimao <caimao@szkingdom.com>
2021-03-19 14:30:23 +0800

    test commit 2

--- src/com/caimao/utils/CommonUtil.java
+++ src/com/caimao/utils/CommonUtil.java
@@ -2,7 +2,7 @@ package com.caimao.utils;
 
 public class CommonUtil {
     public static void main(String[] args) {
-        System.out.println("---------------");
+        System.out.println("---------------");//
     }
 
     
