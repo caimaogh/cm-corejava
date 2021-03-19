cdfd3f15452b8c55f56288b00f67ffde60d1d63d
Cai Mao <caimao@szkingdom.com>
2021-03-19 15:48:02 +0800

    dev commit test

--- src/com/caimao/utils/CommonUtil.java
+++ src/com/caimao/utils/CommonUtil.java
@@ -3,6 +3,7 @@ package com.caimao.utils;
 public class CommonUtil {
     public static void main(String[] args) {
         System.out.println("---------------");//add confirm
+        System.out.println("===============");
     }
 
     
